58ce7c15910c6868d3f6f4891bc0e5f052abc99b
Sabir <bovba179@gmail.com>
2023-02-14 04:16:33 +0300

    Initial workNet

---
+++ Worker.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        Random r = new Random();
+        int caseWithWorker = r.nextInt(2) + 1;
+        if (caseWithWorker == 1) {
+            for (int i = 1; i <= 100; i++) {
+                callback.onDone("Task " + i + " is done");
+            }
+        } else {
+            for (int i = 1; i <= 100; i++) {
+                if (i == 33) System.out.println("Task 33 is failed");
+                errorCallback.onError("Task " + i + " is done");
+            }
+        }
+    }
+}
